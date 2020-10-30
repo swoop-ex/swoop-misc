5dc79e570eda8a85ebb7c0c8a9a7bdcbcfee6e54
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-10-30 16:44:19 +0100

    Downgrade solc/openzeppelin, add proper WBTC contract, change ONEBTC to 8 decimals

--- contracts/extra/OneChainlink.sol
+++ contracts/extra/OneChainlink.sol
@@ -1,18 +1,18 @@
-pragma solidity =0.6.6;
+pragma solidity >=0.4.21 <0.6.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OneChainlink is ERC20PresetMinterPauser, Ownable {
+contract OneChainlink is ERC20, ERC20Mintable, ERC20Detailed {
   using SafeERC20 for IERC20;
   using Address for address;
   using SafeMath for uint;
 
-  constructor() public ERC20PresetMinterPauser("OneChainlink", "1LINK") {
-      _mint(msg.sender, 100000000000000000000000000);
+  constructor() public ERC20Detailed("OneChainlink", "1LINK", 18) {
+    _mint(msg.sender, 100000000000000000000000000);
   }
 }
---
+++ contracts/extra/WBTC/WrappedToken.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract WrappedToken is ERC20,
+  ERC20Mintable, ERC20Burnable, ERC20Pausable, Ownable {
+
+  function burn(uint value) public onlyOwner {
+    super.burn(value);
+  }
+
+  function finishMinting() public view onlyOwner returns (bool) {
+    return false;
+  }
+
+  function renounceOwnership() public onlyOwner {
+    revert("renouncing ownership is blocked");
+  }
+}
